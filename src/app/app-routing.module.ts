e78e465ff4bdfd9f930e5bf4554fe9a39706018c
Bognar <josip.bognar@optinet.hr>
2019-02-21 13:19:32 +0100

    new adds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
+import { WorkComponent } from './pages/work/work.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blog', component:BlogComponent },
+  { path: '', component:HomeComponent },
+  { path: 'work', component:WorkComponent },
+  { path: 'contact', component:ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public polarAreaChartLabels:string[] = ['HTML', 'CSS', 'javaScript', 'Bootstrap', 'jQuery', 'Angular', 'Firebase', 'NodeJS', 'ChartsJS', 'Ionic'];
+  public polarAreaChartData:number[] = [80, 80, 60, 80, 60, 50, 40, 40, 60, 50];
+  public polarAreaLegend:boolean = true;
+  public polarAreaChartType:string = 'polarArea';
+  constructor() { }
+
+ visitor = "Visitor";
+ disable:boolean = false;
+  ngOnInit() {
+  }
+
+  saveData(){
+    this.disable = true;
+    localStorage.setItem('visitor', this.visitor);
+  }
+
+}
+
