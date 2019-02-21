96307c041ef56be14202e66e663169195466f28e
Bognar <josip.bognar@optinet.hr>
2019-02-21 15:56:09 +0100

    updated chartjs options

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,16 @@ export class HomeComponent implements OnInit {
   public polarAreaChartLabels:string[] = ['HTML', 'CSS', 'javaScript', 'Bootstrap', 'jQuery', 'Angular', 'Firebase', 'NodeJS', 'ChartsJS', 'Ionic'];
   public polarAreaChartData:number[] = [80, 80, 60, 80, 60, 50, 40, 40, 60, 50];
   public polarAreaLegend:boolean = true;
+  public polarAreaOptions:any ={
+   legend:{ 
+     position:'right',
+     labels: {
+     boxWidth:10
+     }
+   }
+  };
   public polarAreaChartType:string = 'polarArea';
+  
   constructor() { }
 
  visitor = "Visitor";
